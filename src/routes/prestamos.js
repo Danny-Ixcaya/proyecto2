61819b9642bd25f36379f3a645a2068dd04b1610
Danny <15daixca@gmail.com>
2022-11-09 01:40:59 -0600

    Mejoramiento de diseño en prestamo

--- src/routes/prestamos.js
+++ src/routes/prestamos.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/prestamos', (req, res) => {
+router.get('/prestamos-crear', (req, res) => {
+    res.render('prestamo/prestamo');
+});
+
+router.get('/prestamos-inicio', (req, res) => {
     res.render('prestamo/inicio');
+});
+
+router.get('/prestamos-ver', (req, res) => {
+    res.render('prestamo/ver-prestamo');
 });
 
 module.exports = router;
